083d4265ee9fa8f7553dd6d40ad0c30ae06b2782
Jelena Penava <jelena.penava@fer.hr>
2026-03-18 14:59:21 +0100

    Pocetno stanje

---
+++ Chapter_03_Interrupts/04_Interrupts/include/api/prog_info.h
@@ -0,0 +1,11 @@
+/*! Program info */
+#pragma once
+
+#include <types/basic.h>
+
+/* "programs" */
+int hello_world();
+int interrupts();
+
+#define	hello_world_PROG_HELP	"Print 'Hello world'."
+#define	interrupts_PROG_HELP	"Interrupts demonstration."
---
+++ Chapter_05_Devices/00_Start/include/api/prog_info.h
@@ -0,0 +1,12 @@
+/*! Program info */
+#pragma once
+
+#include <types/basic.h>
+
+/* "programs" */
+int hello_world();
+int timer();
+
+#define	hello_world_PROG_HELP	"Print 'Hello world'."
+#define	timer_PROG_HELP		"Timer interface demonstration: "	\
+				"periodic timer activations."
